fbf24bd5ac5ccd20491964038233d5e537f2794c
Ryan Christian <me@ryanchristian.com>
2021-01-18 12:22:31 -0500

    Automated NER now runs through every txt file in a directory, creates a new folder with the txt files name, and runs all three algorithms on it. Takes 1 argument which is the path to the directory it should go through. Logs are saved to debug.log.

--- Ryan/automated_ner.py
+++ Ryan/automated_ner.py
@@ -1,8 +1,18 @@
 import os
 import sys
 import polyglot
+import logging
 from polyglot.text import Text
 from flair.models import SequenceTagger
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format=('[%(asctime)s] %(levelname)-8s %(message)s'),
+    handlers=[
+        logging.FileHandler('debug.log'),
+        logging.StreamHandler(sys.stdout),
+    ]
+)
 
 flair_12class = SequenceTagger.load('ner-ontonotes-fast')
 flair_4class = SequenceTagger.load('ner')
@@ -24,32 +34,44 @@ def main():
 
     for root, _, files in os.walk(sys.argv[1]):
         for filename in files:
-            if filename.lower() != "readme.txt" and os.path.splitext(filename)[1] == ".txt":
-                path = os.path.join(root, filename)
-                text = "" #Still need to figure out a way to get text
-                '''
-                with open(os.path.join(root, name), "r", encoding='ANSI') as file:
-                    print(os.path.join(root, name))
+            if filename.lower() != "readme.txt" and os.path.splitext(filename)[1] == ".txt" and not filename.startswith("flair4class_") and not filename.startswith("flair12class_") and not filename.startswith("polyglot_"):
+                with open(os.path.join(root, filename), "r", encoding="windows_1252") as file:
                     try:
-                        print(file.read())
-                        print("BLAH")
-                        input()
+                        text = file.read()
                     except Exception as err:
-                        print(err)
-                print(os.path.join(root, name))
-                '''
-                output = flair_ner(text, flair_4class)
-                with open(os.path.join(root, "flair4class_" + filename), 'w') as file:
-                    file.writelines("%s\n" % entity for entity in output)
-                    
-                output = flair_ner(text, flair_12class)
-                with open(os.path.join(root, "flair12class_" + filename), 'w') as file:
-                    file.writelines("%s\n" % entity for entity in output)
+                        logging.error("Unable to read file '" + os.path.join(root, filename) + "': " + str(err))
+                        continue
+                        
+                if os.path.basename(os.path.normpath(root)) != os.path.splitext(filename)[0]:
+                    tmp = os.path.join(root, os.path.splitext(filename)[0])
+                    os.mkdir(tmp)
+                    os.rename(os.path.join(root, filename), os.path.join(tmp, filename))
+                    root = tmp
 
-                output = polyglot_ner(text)
-                with open(os.path.join(root, "polyglot_" + filename), 'w') as file:
-                    file.writelines("%s\n" % entity for entity in output)
-                input()
+                print("Running flair4class algorithm on '" + filename + "'...")
+                try:
+                    with open(os.path.join(root, "flair4class_" + filename), 'w') as file:
+                        file.writelines("%s\n" % str(entity) for entity in flair_ner(text, flair_4class))
+                    print("Completed!")
+                except Exception as err:
+                    logging.error("flair4class failed on '" + os.path.join(root, filename) + "': " + str(err))
+                
+                print("Running flair12class algorithm on '" + filename + "'...")
+                try:
+                    with open(os.path.join(root, "flair12class_" + filename), 'w') as file:
+                        file.writelines("%s\n" % str(entity) for entity in flair_ner(text, flair_12class))
+                    print("Completed!")
+                except Exception as err:
+                    logging.error("flair12class failed on '" + os.path.join(root, filename) + "': " + str(err))
+
+                print("Running polyglot algorithm on '" + filename + "'...")
+                try:
+                    with open(os.path.join(root, "polyglot_" + filename), 'w') as file:
+                        file.writelines("%s\n" % str(entity) for entity in polyglot_ner(text))
+                    print("Completed!")
+                except Exception as err:
+                    logging.error("polyglot failed on '" + os.path.join(root, filename) + "': " + str(err))
+                print()
 
 if __name__ == "__main__":
     main()
